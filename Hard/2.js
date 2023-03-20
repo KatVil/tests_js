f7591d604c17a6ba9d956adf399fb567cde7853f
Katherine Vilinski-Mazur <Katherine.Vilinski-Mazur@skoltech.ru>
2023-03-20 16:58:55 +0300

    taskH-2-qstn

--- Hard/2.js
+++ Hard/2.js
@@ -10,10 +10,22 @@
 Пример: [ 'one', 'two', 'three' ] => 'three'
 
 */
-
-const longestString = () => {
-    /* Тут код*/
-
+let arr = [ 'one', 'two', 'three' ];
+const longestString = (arr) => {
+  let longest = arr[0];
+  for(let i = 0; i < arr.length; i++){
+    if((arr[i].length > longest) && (arr!=null)){
+        longest = arr[i];
+        console.log('????');
+    }
+    else if (arr==[]){
+        longest = null;
+    }
+    else if (arr[i]===arr[(i-1)]){
+        longest = arr[(i-1)];
+    }
+  }
+  return longest;
 }
-
+console.log(longestString([ 'one', 'two', 'three' ]));
 module.exports = longestString
